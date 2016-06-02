89f9d8540672b70c1d180b184c4086e84fbe23a0
ken <xuanzhao89@gmail.com>
2016-06-03 02:06:50 +0900

    QLSVM_RF change to class version

--- code3/my_QLSVM_RF.py
+++ code3/my_QLSVM_RF.py
@@ -593,81 +593,16 @@ class DecisionTreeRegresion(object):
 
 
 
-# ============================RandomFrestRegression===========================
-class RandomForestRegression(object):
-    """A random forest classifier.
+# ============================RandomFrestClassification===========================
 
-    A random forest is a meta estimator that fits a number of decision tree
-    classifiers on various sub-samples of the dataset and use averaging to 
-    improve the predictive accuracy and control over-fitting.
-    The sub-sample size is always the same as the original input sample size
-    but the samples are drawn with replacement if `bootstrap=True`(default).
+class QLSVM_clf_RF(object):
 
-    Parameters
-    ----------
-    n_trees: integer, optional(default=10)
-        The number of trees in the forest.
-
-    errType: 
-
-    leaftype:
-
-    max_features:
-
-    max_depth:
-
-    min_samples_split:
-
-    min_weight_fraction_leaf:
-
-    random_state:
-
-    class_weight:
-
-    bootstrap : boolean, optional (default=True)
-        Whether bootstrap samples are used when building trees.
-
-    oob_score : bool
-        Whether to use out-of-bag samples to estimate the generalization error.
-
-    n_jobs : integer, optional (default=1)
-        The number of jobs to run in parallel for both `fit` and `predict`.
-        If -1, then the number of jobs is set to the number of cores.
-
-
-    Attributes
-    ----------
-    estimators : list of DecisionTreeClassifier
-        The collection of fitted sub-estimators.
-
-    n_features : int
-        The number of features when ``fit`` is performed.
-
-    n_outputs : int
-        The number of outputs when ``fit`` is performed.
-
-    classes : array of shape = [n_classes] or a list of such arrays.
-
-    oob_score : float
-        Score of the training dataset obtained using an out-of-bag estimate.
-    """
-
-    def __init__(self,
-                 n_trees=10,
-                 errType='lseErr',
-                 leafType='ridgeRegr',
-                 max_depth=3,
-                 min_samples_split=5,
-                 min_weight_fraction_leaf=0.0,
-                 max_features=None,
-                 random_state=None,
-                 class_weight=None,
-                 bootstrap=True,
-                 oob_score=False,
-                 n_jobs=1,
-                 ):
-
-    # user's input attributes
+    def __init__(self, n_trees=10,
+                errType='lseErr_regul',leafType='LogicReg',
+                max_depth=5, min_samples_split=10, max_features=None,
+                min_weight_fraction_leaf=0.0,
+                random_state=None, class_weight=None):
+    
         self.n_trees = n_trees
         self.errType = errType
         self.leafType = leafType
@@ -676,165 +611,157 @@ class RandomForestRegression(object):
         self.min_weight_fraction_leaf = min_weight_fraction_leaf
         self.max_features = max_features
         self.random_state = random_state
-        self.class_weight = class_weight
+        self.class_weight = class_weight    
 
-    # decisionTree privite attributes, which is determinated by input data
-        # self.n_features = None
-        # self.n_outputs  = None
-        # self.classes    = None
 
-        self.trees = []
-        for n in range(n_trees):
-            self.trees.append(DecisionTreeRegresion(
-                            errType = ERRTYPE.get(errType), # here not pass para, ERRTYPE not defined
-                            leafType = LEAFTYPE.get(leafType),
-                            max_depth = max_depth,
-                            min_samples_split = min_samples_split,
-                            min_weight_fraction_leaf = min_weight_fraction_leaf,
-                            max_features = max_features,
-                            random_state = random_state,
-                            class_weight = class_weight)
-                            )
-
- 
     def fit(self, X_train, y_train):
-        for tree in self.trees:
-            tree.fit(X_train, y_train)
         
-        return self
+        # LEAFTYPE = {'SGDClf': SGDClf, 'LogicReg': LogicReg, 'RidgeReg': RidgeReg, 
+                     # 'RANSACReg': RANSACReg, 'BayesReg': BayesReg,
+                     # 'IsotonicReg': IsotonicReg, 'KernelRidge':KernelRidge
+                     # }
+        # ERRTYPE = {'lseErr': lseErr, 'lseErr_regul': lseErr_regul}
 
-    def predict(self,X_test):
-        predictions = []
-        for tree in self.trees:
-            y_pred = tree.predict(X_test)
-            predictions.append(y_pred)
+        from sklearn.utils import resample
+        trees = []
+        for n in range(self.n_trees):
+            trees.append(DecisionTreeRegresion(
+                         errType=self.errType,
+                         leafType=self.leafType,
+                         max_depth=self.max_depth,
+                         min_samples_split=self.min_samples_split,
+                         min_weight_fraction_leaf=self.min_weight_fraction_leaf,
+                         max_features=self.max_features,
+                         random_state=self.random_state,
+                         class_weight=self.class_weight)
+                        )
+        m,n = X_train.shape
+        data_oob_List = []
         
-        avg_pred = np.mean(predictions, axis=1)
-        return avg_pred
+        for tree in trees:
+
+            # get random features
+            feat_ind = np.random.choice(n, int(0.7*n), replace=False)
+            # get data samples
+            X_boot_train, y_boot_train = resample(X_train, y_train)
+            
+            # get oob data samples
+            boot_ind = np.in1d(X_train[:,0], X_boot_train[:,0])
+            X_oob_train = X_train[~boot_ind]
+            y_oob_train = y_train[~boot_ind]
+            data_oob_List.append(np.c_[X_oob_train, y_oob_train])
+
+            tree.fit(X_boot_train, y_boot_train)
+
+        self.trees = trees
+        self.data_oob_List = data_oob_List
+        return self  # type is list
+
+    def RF_predict(self, X_test):
+        predictions = []  # len=m
+        trees = self.trees
+
+        for tree in trees:
+            y_pred = tree.predict(X_test)  #(m,1)
+            predictions.append(y_pred)      
+
+        predictions = np.array(predictions)  # (n,m,1) , n is number of trees
+        avg_pred = np.mean(predictions, axis=0) #(m,1)
+        sigmoid_pred = np.where(avg_pred>0.5, 1, 0)
+        
+        return sigmoid_pred
 
 
-def RF_fit(X_train, y_train, n_trees=10,
-            errType='lseErr_regul',leafType='LogicReg',
-            max_depth=5, min_samples_split=10, max_features=None):
-    
+    def get_QLSVM_RF(self, X_train, y_train, lamb=2):
 
-    # LEAFTYPE = {'SGDClf': SGDClf, 'LogicReg': LogicReg, 'RidgeReg': RidgeReg, 
-                 # 'RANSACReg': RANSACReg, 'BayesReg': BayesReg,
-                 # 'IsotonicReg': IsotonicReg, 'KernelRidge':KernelRidge
-                 # }
-    # ERRTYPE = {'lseErr': lseErr, 'lseErr_regul': lseErr_regul}
+        import get_Quasi_linear_Kernel
+        from functools import partial
+        from sklearn.svm import SVC
+        import scipy as sp
+        from sklearn.grid_search import RandomizedSearchCV
 
-    from sklearn.utils import resample
-    trees = []
-    for n in range(n_trees):
-        trees.append(DecisionTreeRegresion(
-                     errType=errType,
-                     leafType=leafType,
-                     max_depth=max_depth,
-                     min_samples_split=min_samples_split,
-                     min_weight_fraction_leaf=0.0,
-                     max_features=max_features,
-                     random_state=None,
-                     class_weight=None)
-                    )
-    m,n = X_train.shape
-    for tree in trees:
+        trees = self.trees
+        RBFinfo_list = []
+        QLSVM_List = []
+        f1_scores = []
+        data_oob_List = self.data_oob_List
+        QL_SVM_param_dist= {'kernel': ['precomputed'],
+                        'C': sp.stats.expon(scale=1000)}
 
-        # get random features
-        feat_ind = np.random.choice(n, int(0.7*n), replace=False)
-        # get data samples
-        X_boot_train, y_boot_train = resample(X_train, y_train)
-        tree.fit(X_boot_train, y_boot_train)
+        # get QLSVM_List and RF_weights
+        for i,tree in enumerate(trees):
 
-    return trees  # type is list
-
-def RF_predict(X_test,trees):
-    predictions = []  # len=m
-    for tree in trees:
-        y_pred = tree.predict(X_test)  #(m,1)
-        predictions.append(y_pred)      
-
-    predictions = np.array(predictions)  # (n,m,1) , n is number of trees
-    avg_pred = np.mean(predictions, axis=0) #(m,1)
-    sigmoid_pred = np.where(avg_pred>0.5, 1, 0)
-    return sigmoid_pred
-    
-
-def get_RF_weights(X_test, y_test, trees):
-
-    f1_scores = []
-    for tree in trees:
-        y_pred = tree.predict(X_test) # (m,1)
-        f1_scores.append(metrics.f1_score(y_test, y_pred))
-
-    f1_scores = np.array(f1_scores)
-    sum_f1_score = np.sum(f1_scores)
-    weights = np.true_divide(f1_scores, sum_f1_score)
-    RF_weights = np.nan_to_num(weights)
-
-    return RF_weights  # type is np.array, (m_tree) 
+            # get tree's RMat
+            RMat = np.array(tree.tree.get_RList()) # (m,n,2)
+            # get QL kernel matrix
+            RBFinfo = partial(get_Quasi_linear_Kernel.get_RBFinfo,RMat=RMat,lamb=lamb)
+            Quasi_linear_kernel = partial(get_Quasi_linear_Kernel.get_KernelMatrix,RMat=RMat)
+            K_train = Quasi_linear_kernel(X_train,X_train) # for training SVM
+            # run randomized search get best QL SVM
+            clf = SVC(kernel='precomputed')
+            n_iter_search = 50
+            random_search = RandomizedSearchCV(clf, param_distributions=QL_SVM_param_dist,
+                                           n_iter=n_iter_search)
+            random_search.fit(K_train, y_train)
+            # print("Random_search Best estimator is :\n"), random_search.best_estimator_
+            QLSVM_List.append(random_search.best_estimator_)
 
 
-def get_QLSVM_RF(trees, X_train, y_train, lamb=10):
-
-    import get_Quasi_linear_Kernel
-    from functools import partial
-    from sklearn.svm import SVC
-    import scipy as sp
-
-    RBFinfo_list = []
-    QLSVM_List = []
-    QL_SVM_param_dist= {'kernel': ['precomputed'],
-                    'C': sp.stats.expon(scale=1000)}
-
-    for tree in trees:
-        # get tree's RMat
-        RMat = np.array(tree.tree.get_RList()) # (m,n,2)
-        # get QL kernel matrix
-        RBFinfo = partial(get_Quasi_linear_Kernel.get_RBFinfo,RMat=RMat,lamb=lamb)
-        Quasi_linear_kernel = partial(get_Quasi_linear_Kernel.get_KernelMatrix,RMat=RMat)
-        K_train = Quasi_linear_kernel(X_train,X_train)
-        #K_test = Quasi_linear_kernel(X_test,X_train)
-        clf = SVC(kernel='precomputed')
-        # run randomized search get best QL SVM
-        n_iter_search = 50
-        random_search = RandomizedSearchCV(clf, param_distributions=QL_SVM_param_dist,
-                                       n_iter=n_iter_search)
-        random_search.fit(K_train, y_train)
-        print("Random_search Best estimator is :\n"), random_search.best_estimator_
-        QLSVM_List.append(random_search.best_estimator_)
-
-    return QLSVM_List
-
-def QLSVM_predict(X_test, X_train, trees, QLSVM_List, RF_weights,lamb=2):
-
-    import get_Quasi_linear_Kernel
-    from functools import partial
-    from sklearn.svm import SVC
-
-    y_pred = np.zeros((len(X_test),len(QLSVM_List)))
-
-    for i,tree in enumerate(trees):
-
-        RMat = np.array(tree.tree.get_RList()) # (m,n,2)
-        RBFinfo = partial(get_Quasi_linear_Kernel.get_RBFinfo,RMat=RMat,lamb=lamb)
-        Quasi_linear_kernel = partial(get_Quasi_linear_Kernel.get_KernelMatrix,RMat=RMat)
-
-        K_test = Quasi_linear_kernel(X_test,X_train)
-        y_pred[:,i] = QLSVM_List[i].predict(K_test) * RF_weights[i]
-
-    final_y_pred_prob = np.sum(y_pred, axis=1)  
-    final_y_pred = np.where(final_y_pred_prob>0.5, 1, 0)
-
-    return final_y_pred
+            # get oob test data, K_oob kernel matrix 
+            data_oob = data_oob_List[i]
+            X_oob = data_oob[:,:-1]; y_oob = data_oob[:,-1]
+            K_oob = Quasi_linear_kernel(X_oob,X_train)     # for get SVM weight
+            oob_pred = random_search.best_estimator_.predict(K_oob) # (m,1)
+            print '\nQLSVM number %d get f1_scores %f\n' % (i, metrics.f1_score(y_oob, oob_pred))
+            f1_scores.append(metrics.f1_score(y_oob, oob_pred))
 
 
+        f1_scores = np.array(f1_scores)
+        sum_f1_score = np.sum(f1_scores)
+        weights = np.true_divide(f1_scores, sum_f1_score)
+        RF_weights = np.nan_to_num(weights)
+        self.RF_weights = RF_weights
+        print '*'*100
+        print 'done get trees_weights :', RF_weights # np.array,(m_tree) 
+        print '*'*100
+
+        self.QLSVM_List = QLSVM_List
+        self.QLSVM_lamb = lamb
+        self.QLSVM_X_train = X_train
+        print '*'*100
+        print 'done get QLSVM_List : ', QLSVM_List 
+        print '*'*100
 
 
+    def QLSVM_predict(self, X_test):
+        '''QLSVM_predict
+        '''
+        import get_Quasi_linear_Kernel
+        from functools import partial
+        from sklearn.svm import SVC
+
+        trees = self.trees
+        QLSVM_List = self.QLSVM_List
+        RF_weights = self.RF_weights
+        X_train = self.QLSVM_X_train
+        lamb = self.QLSVM_lamb
 
 
+        y_pred = np.zeros((len(X_test),len(QLSVM_List)))
 
+        for i,tree in enumerate(trees):
 
+            RMat = np.array(tree.tree.get_RList()) # (m,n,2)
+            RBFinfo = partial(get_Quasi_linear_Kernel.get_RBFinfo,RMat=RMat,lamb=lamb)
+            Quasi_linear_kernel = partial(get_Quasi_linear_Kernel.get_KernelMatrix,RMat=RMat)
+
+            K_test = Quasi_linear_kernel(X_test,X_train)
+            y_pred[:,i] = QLSVM_List[i].predict(K_test) * RF_weights[i]
+
+        final_y_pred_prob = np.sum(y_pred, axis=1)  
+        final_y_pred = np.where(final_y_pred_prob>0.5, 1, 0)
+
+        return final_y_pred
 
 
 
