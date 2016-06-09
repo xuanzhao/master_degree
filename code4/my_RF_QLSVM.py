5f554cb61737ad2c7e05dbfac87e727cf1939a71
ken <xuanzhao89@gmail.com>
2016-06-09 15:07:14 +0900

    change error fun

--- code4/my_RF_QLSVM.py
+++ code4/my_RF_QLSVM.py
@@ -66,11 +66,12 @@ def lseErr_regul(X, y, leafType, k=0.5):
             # print 'now use predict method, leaf model is \n',model
             yHat = model.predict(X)
         
-        X1_mean = np.mean(X[y==1], axis=0)
-        X0_mean = np.mean(X[y==0], axis=0)
-        X1_delta = X[y==1] - X0_mean # (m,n)
-        X0_delta = X[y==0] - X1_mean
-        X_delta = np.r_[X1_delta, X0_delta]
+        # X1_mean = np.mean(X[y==1], axis=0)
+        # X0_mean = np.mean(X[y==0], axis=0)
+        # X1_delta = X[y==1] - X0_mean # (m,n)
+        # X0_delta = X[y==0] - X1_mean
+        # X_delta = np.r_[X1_delta, X0_delta]
+        X_delta = X - np.mean(X, axis=0)
         error = (np.sum(np.power(y[:,np.newaxis] - yHat, 2))  + \
                 k * np.sum(np.power(X_delta, 2)) ) /len(yHat)
 
